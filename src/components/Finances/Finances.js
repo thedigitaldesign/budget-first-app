e6bfe8a60626b98f7d22686d44c31b51879b88e3
OneJChristensen <joshua@thedigitaldesign.com>
2019-05-27 22:43:27 -0600

    Getting project setup

---
+++ src/components/Finances/Finances.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Finances extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default Finances
