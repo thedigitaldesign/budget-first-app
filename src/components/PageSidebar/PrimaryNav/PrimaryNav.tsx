4c37bb87ee6f3cdf9c45c3fa48cb4672e77d1682
Joshua Christensen <joshua@thedigitaldesign.com>
2020-03-12 23:42:35 -0600

    Moved the nav and stopped to learn more about TDD

--- src/components/PageSidebar/PrimaryNav/PrimaryNav.tsx
+++ src/components/PageSidebar/PrimaryNav/PrimaryNav.tsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
 // Axios
+import axios from 'axios'
 
 export default class PrimaryNav extends Component {
   render() {
+    let nav
+
+    axios.get('./nav.json')
+      .then(response => {
+        nav = response.data.nav
+      })
+
+    console.log()
+
     return (
       <nav className="primary-nav">
         
